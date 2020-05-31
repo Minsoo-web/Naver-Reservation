01299e163b061c0c0af6c90475c81b30f7756ab3
Minsoo <zlemzlem5656@naver.com>
2020-06-01 02:45:16 +0900

    docs : button ui 구성
    <description>
    색상 (배경/ 글자색), type을 props로 받게끔 설정
    hover 이벤트를 메서드로 색상 변경 구현

---
+++ naver-vue/src/components/UI-Components/Button.vue
@@ -0,0 +1,53 @@
+<template>
+  <button :style="styleObj" :type="type" @mouseover="onHover" @mouseleave="onLeave">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  data() {
+    return {
+      styleObj: {
+        backgroundColor: this.bgColor,
+        color: this.txtColor
+      }
+    };
+  },
+  methods: {
+    // 호버시 색상 변경
+    onHover(e) {
+      let colorArr = this.bgColor.split(",");
+      let hoverColor = parseInt(colorArr[1]) + 10;
+      colorArr[1] = hoverColor + "";
+      e.target.style.backgroundColor = colorArr.join(",");
+    },
+    onLeave(e) {
+      e.target.style.backgroundColor = this.bgColor;
+    }
+  },
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    bgColor: {
+      type: String,
+      default: "rgb(123,207,163)"
+    },
+    txtColor: {
+      type: String,
+      default: "#ffffff"
+    },
+    type: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  padding: 10px;
+}
+</style>
