122fca34156214ba85b44214d1a971f5e4c9ff95
Minsoo <zlemzlem5656@naver.com>
2020-06-01 02:44:06 +0900

    docs : views 구성
    <description>
    우선은 렌딩 페이지와 로그인 페이지 구성을 목표로 함

--- naver-vue/src/views/Home.vue
+++ naver-vue/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <h1>Hello</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home"
+};
 </script>
+
+<style scoped>
+#home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(38, 60, 86);
+}
+</style>
---
+++ naver-vue/src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="login">
+    <div class="container">
+      <h1>Login</h1>
+      <Form />
+    </div>
+  </div>
+</template>
+
+<script>
+import Form from "@/components/form/Form";
+export default {
+  components: {
+    Form
+  }
+};
+</script>
+
+<style scoped>
+#login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(38, 60, 86);
+}
+.container {
+  width: 50%;
+  height: 50%;
+  padding: 20px;
+}
+h1 {
+  width: 100%;
+  text-align: start;
+  color: #ffffff;
+}
+</style>
