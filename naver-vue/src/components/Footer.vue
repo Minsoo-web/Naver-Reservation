e13bea4ed606e7720942c4b9d6ef58e304893795
Minsoo <zlemzlem5656@naver.com>
2020-06-01 02:42:42 +0900

    docs : 섹션 컴포넌트 구성
    <description>
    우선 nav / footer 만 컴포넌트 화 했으며,
    content 영역은 router-view 로 대체할 생각

---
+++ naver-vue/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>footer</footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style>
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 10px;
+  background-color: #35495e;
+  color: #ffffff;
+}
+</style>
---
+++ naver-vue/src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <router-link :to="{ name : 'Home'}">Home</router-link>
+    <router-link :to="{ name : 'Login'}">Login</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+nav {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  background-color: rgb(38, 60, 86);
+  display: flex;
+  padding: 20px;
+}
+a {
+  color: #ffffff;
+  text-decoration: none;
+}
+</style>
