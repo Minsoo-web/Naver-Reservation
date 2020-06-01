b969395962a01a30ab58669a4f8666f877b14591
Minsoo <zlemzlem5656@naver.com>
2020-06-01 23:04:48 +0900

    docs :  Nav 수정
    <description>
    로그인 여부에 따른 동적 구현
    v-menu를 통한 드롭다운 구현
    
    <issue-number>
    close #1

--- naver-vue/src/components/Nav.vue
+++ naver-vue/src/components/Nav.vue
@@ -1,15 +1,43 @@
 <template>
-  <nav>
-    <router-link :to="{ name : 'Home'}">Home</router-link>
-    <router-link :to="{ name : 'Login'}">Login</router-link>
+  <nav data-app>
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+    <router-link :to="{ name: 'SignUp' }">Sign Up</router-link>
+
+    <v-menu offset-y v-if="isLogin">
+      <template v-slot:activator="{ on }">
+        <v-btn depressed color="teal darken-1" dark v-on="on">
+          Dropdown
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item router :to="{ name: 'Mypage' }">
+          <v-list-item-title>마이페이지</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="logout">
+          <v-list-item-title>로그아웃</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <router-link v-else :to="{ name: 'Login' }">Login</router-link>
   </nav>
 </template>
 
 <script>
-export default {};
+import { mapState, mapActions } from "vuex";
+export default {
+  computed: {
+    ...mapState(["isLogin"]),
+  },
+  methods: {
+    ...mapActions(["logout"]),
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
-<style>
+<style scope>
 nav {
   position: fixed;
   width: 100%;
@@ -18,7 +46,7 @@ nav {
   display: flex;
   padding: 20px;
 }
-a {
+nav a {
   color: #ffffff;
   text-decoration: none;
 }
