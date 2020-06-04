8a77c223e1da5aac087a880358b76963a1053805
Minsoo <zlemzlem5656@naver.com>
2020-06-05 01:51:19 +0900

    docs : content 추가
    <description>
    가맹점 리스트를 보여주는 일은 앞으로도
    허다하게 많을 예정이니
    컴포넌트화 해놓는게 좋겠다.

--- naver-vue/src/components/Mypage/Content.vue
+++ naver-vue/src/components/Mypage/Content.vue
@@ -1,14 +1,39 @@
 <template>
   <section>
-    <h1>contents</h1>
+    <div class="shopInfo-card" v-for="shop in reserveInfo[0].shopInfo" :key="shop.id">
+      <div class="thumnail-wrapper">
+        <img :src="shop.thumnail" alt />
+      </div>
+      <span>{{ shop.name }}</span>
+      <span>{{ shop.date }}</span>
+      <span>{{ shop.score }}</span>
+    </div>
   </section>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-  name: "Content"
+  name: "Content",
+  computed: {
+    ...mapState(["reserveInfo"])
+  }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+section {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  .shopInfo-card {
+    display: flex;
+    flex-direction: column;
+    flex-basis: 33%;
+    .thumnail-wrapper {
+      width: 100%;
+      height: 50%;
+    }
+  }
+}
 </style>
