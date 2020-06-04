93836729436d5661b3e4192735efa866c6a5420b
Minsoo <zlemzlem5656@naver.com>
2020-06-05 01:52:53 +0900

    docs : 컴포넌트 이름 변경
    <description>
    리뷰 탭을 따로 만드는 게 아니라
    tab-nav를 따로 만들어서 컨트롤 하는 것이
    낫다고 판단

---
+++ naver-vue/src/components/Mypage/TabNav.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="TabNav">
+    <ul>
+      <li v-for="shop in reserveInfo" :key="shop.title">{{shop.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "TabNav",
+  computed: {
+    ...mapState(["reserveInfo"])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#TabNav {
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
+  border-top: 1px solid;
+  ul {
+    display: flex;
+    li {
+      /* border: 1px solid; */
+      padding: 10px 40px;
+    }
+  }
+}
+</style>
