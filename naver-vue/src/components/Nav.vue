d1ecd8251f9c869ae5b2a7f37cbd8c920a7f02c9
Minsoo <zlemzlem5656@naver.com>
2020-06-06 02:32:31 +0900

    docs : Nav 디자인 추가
    <description>
    로그인 후 회원가입 창 안 보이게
    Nav도 80% 후 가운데 정렬

--- naver-vue/src/components/Nav.vue
+++ naver-vue/src/components/Nav.vue
@@ -1,24 +1,29 @@
 <template>
   <nav data-app>
-    <router-link :to="{ name: 'Home' }">Home</router-link>
-    <router-link :to="{ name: 'SignUp' }">Sign Up</router-link>
-
-    <v-menu offset-y v-if="isLogin">
-      <template v-slot:activator="{ on }">
-        <v-btn depressed color="teal darken-1" dark v-on="on">
-          Dropdown
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item router :to="{ name: 'Mypage' }">
-          <v-list-item-title>마이페이지</v-list-item-title>
-        </v-list-item>
-        <v-list-item @click="logout">
-          <v-list-item-title>로그아웃</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-    <router-link v-else :to="{ name: 'Login' }">Login</router-link>
+    <ul id="Nav">
+      <li>
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+      </li>
+      <li>
+        <v-menu offset-y v-if="isLogin">
+          <template v-slot:activator="{ on }">
+            <v-btn depressed color="teal darken-1" dark v-on="on">Dropdown</v-btn>
+          </template>
+          <v-list>
+            <v-list-item router :to="{ name: 'Mypage' }">
+              <v-list-item-title>마이페이지</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="logout">
+              <v-list-item-title>로그아웃</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <div v-else>
+          <router-link :to="{ name: 'Login' }">Login</router-link>
+          <router-link :to="{ name: 'SignUp' }">Sign Up</router-link>
+        </div>
+      </li>
+    </ul>
   </nav>
 </template>
 
@@ -26,28 +31,34 @@
 import { mapState, mapActions } from "vuex";
 export default {
   computed: {
-    ...mapState(["isLogin"]),
+    ...mapState(["isLogin"])
   },
   methods: {
-    ...mapActions(["logout"]),
+    ...mapActions(["logout"])
   },
   data() {
     return {};
-  },
+  }
 };
 </script>
 
-<style scope>
+<style lang="scss" scoped>
 nav {
   position: fixed;
-  width: 100%;
   top: 0;
+  width: 100%;
+  padding: 20px 0;
   background-color: rgb(38, 60, 86);
-  display: flex;
-  padding: 20px;
-}
-nav a {
-  color: #ffffff;
-  text-decoration: none;
+  #Nav {
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+  }
+  a {
+    color: #ffffff;
+    text-decoration: none;
+  }
 }
 </style>
