2fd3b63f95b15710378afc3ac4d4b3fcd32d18e1
Minsoo <zlemzlem5656@naver.com>
2020-06-09 23:39:38 +0900

    docs : views 컴포넌트 생성
    <description>
    account/edit 페이지와
    account/password/change 페이지의
    컴포넌트 생성 및 디자인

--- naver-vue/src/views/Account/Edit.vue
+++ naver-vue/src/views/Account/Edit.vue
@@ -1,22 +1,79 @@
 <template>
-  <div>
-    <Profile :picture="userInfo.picture" :size="'72px'" />
+  <div id="Edit-wrapper">
+    <!-- row1 -->
+    <LabelInput>
+      <template v-slot:col1>
+        <Profile :picture="userInfo.picture" :size="'38px'" />
+      </template>
+      <template v-slot:col2>
+        <strong>{{userInfo.email}}</strong>
+        <Modal :text="'프로필 사진 바꾸기'" :type="'text'"></Modal>
+      </template>
+    </LabelInput>
+    <!-- row2 -->
+    <LabelInput>
+      <template v-slot:col1>
+        <strong>이름</strong>
+      </template>
+      <template v-slot:col2>
+        <input type="text" :value="userInfo.name" class="input" @keyup="onChange" />
+      </template>
+    </LabelInput>
+    <div class="submit-wrapper">
+      <Button
+        :type="'btn'"
+        :text="'변경'"
+        :disabled="change"
+        @onClick="userEdit({
+        property : 'name',
+        value : name
+      })"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import Profile from "@/components/UI-Components/Profile";
-import { mapState } from 'vuex';
+import Modal from "@/components/UI-Components/Modal";
+import LabelInput from "@/components/form/LabelInput";
+import Button from "@/components/UI-Components/Button";
+import { mapState, mapActions } from "vuex";
 export default {
   name: "Edit",
   components: {
-    Profile
+    Profile,
+    Modal,
+    LabelInput,
+    Button
   },
-  computed:{
-    ...mapState(['userInfo'])
+  computed: {
+    ...mapState(["userInfo"])
+  },
+  data() {
+    return {
+      change: true,
+      name: ""
+    };
+  },
+  methods: {
+    ...mapActions(["userEdit"]),
+    onChange(e) {
+      this.change = false;
+      this.name = e.target.value;
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+#Edit-wrapper {
+  width: 100%;
+  .submit-wrapper {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    padding: 0 40px;
+  }
+}
 </style>
--- naver-vue/src/views/Account/Password.vue
+++ naver-vue/src/views/Account/Password.vue
@@ -1,12 +1,77 @@
 <template>
-  <h1>password</h1>
+  <div id="Edit-wrapper">
+    <LabelInput>
+      <template v-slot:col1>
+        <strong>현재 비밀번호</strong>
+      </template>
+      <template v-slot:col2>
+        <input type="password" class="input" />
+      </template>
+    </LabelInput>
+    <LabelInput>
+      <template v-slot:col1>
+        <strong>변경할 비밀번호</strong>
+      </template>
+      <template v-slot:col2>
+        <input type="password" class="input" v-model="password" />
+      </template>
+    </LabelInput>
+    <LabelInput>
+      <template v-slot:col1>
+        <strong>비밀번호 확인</strong>
+      </template>
+      <template v-slot:col2>
+        <input type="password" class="input" v-model="re_password" />
+      </template>
+    </LabelInput>
+    <div class="submit-wrapper">
+      <Button
+        :type="'btn'"
+        :text="'변경'"
+        :disabled="!isPassword"
+        @onClick="userPwEdit({
+          password : password
+        })"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import Button from "@/components/UI-Components/Button";
+import LabelInput from "@/components/form/LabelInput";
+import { mapActions } from "vuex";
 export default {
-  name: "Password"
+  name: "Password",
+  components: {
+    LabelInput,
+    Button
+  },
+  computed: {
+    isPassword() {
+      return this.password == this.re_password;
+    }
+  },
+  data() {
+    return {
+      password: "",
+      re_password: ""
+    };
+  },
+  methods: {
+    ...mapActions(["userPwEdit"])
+  }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+#Edit-wrapper {
+  width: 100%;
+  .submit-wrapper {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    padding: 0 40px;
+  }
+}
 </style>
