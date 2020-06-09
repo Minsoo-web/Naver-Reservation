100736ac893ac7e3daa74ea22b657c7f99149d77
Minsoo <zlemzlem5656@naver.com>
2020-06-09 23:37:05 +0900

    docs : ui 컴포넌트 생성
    <description>
    라벨 - input 구조가 반복 되는 경우를 위해
    컴포넌트화 했습니다.

---
+++ naver-vue/src/components/form/LabelInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="edit-row">
+    <div class="edit-col1">
+      <slot name="col1"></slot>
+    </div>
+    <div class="edit-col2">
+      <slot name="col2"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.edit-row {
+  display: flex;
+  width: 100%;
+  height: 70px;
+  padding: 10px 0;
+  /* border-bottom: 1px solid; */
+  .edit-col1 {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    min-width: 100px;
+  }
+  .edit-col2 {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1;
+    padding: 0 40px;
+  }
+}
+</style>
