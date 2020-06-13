00b29ec404cff30214c72babe3b8e2dca8bf00a0
Minsoo <zlemzlem5656@naver.com>
2020-06-13 16:58:24 +0900

    bugs : 이슈 # 10 에러 핸들링
    <description>
    publicPath 추가
    @ alias 추가
    
    <issue-number>
    open # 10

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const path = require("path");
 module.exports = {
   transpileDependencies: ["vuetify"],
+
+  publicPath: "/",
+
+  configureWebpack: {
+    // resolve.alias는 모듈을 더 쉽게 import, require 하게 만듭니다.
+    resolve: {
+      alias: {
+        // '@'는 현재 프로젝트의 client/src/까지의 최종 경로를 의미합니다.
+        "@": path.join(__dirname, "src/"),
+      },
+    },
+  },
 
   devServer: {
     overlay: {
@@ -7,6 +20,4 @@ module.exports = {
       errors: true,
     },
   },
-
-  publicPath: "",
 };
