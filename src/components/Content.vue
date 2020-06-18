1b71c3734eb3e893ed28f1561ecb392a73f2a91f
Minsoo <zlemzlem5656@naver.com>
2020-06-18 15:41:37 +0900

    docs : content component 추가
    <description>
    자주 사용되는 content wrapping
    component 를 만들었습니다.

---
+++ src/components/Content.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="content">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Content"
+};
+</script>
+
+<style lang="scss" scoped>
+#content {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 82vh;
+  margin-top: 76px;
+}
+</style>
